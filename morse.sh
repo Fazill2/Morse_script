49b5d0f0523b5e0dfc0bd1cf6abeec1bb82880ca
Fazill2 <100223031+Fazill2@users.noreply.github.com>
2022-05-23 01:35:20 +0200

    Update morse.sh

--- morse.sh
+++ morse.sh
@@ -45,26 +45,35 @@ if (("$#" == "0")); then
         echo "help"
         exit
 fi
-
+filename=""
 while (("$#" > 0)); do
         if test "$1" = "-h"; then
                 echo "help"
                 echo "$1"
-        elif test "$1" = "-f"; then
+        elif test "$1" = "-t"; then
                 shift
-                echo "$1"
+                filename="$1"
         else
                 foo="$1"
+                if test "$foo" = "-f"; then
+                        shift
+                        if test -r "$1"; then
+                                foo=$(cat "$1")
+                        else
+                                echo "Could not open the file"
+                                foo=""
+                        fi
+                fi
                 for ((i=0;i<${#foo};i++)); do
                         c=${foo:$i:1}
                         c=${c^^}
                         if test "$c" = " ";then
                                 printf '       '
                         else
-                                printf '%s ' "${morse[$c]}"
+                                printf '%s ' "${morse[$c]}" | tee -a "$filename" 2> /dev/null
                         fi
                 done
-                printf "\n"
+                printf "\n" | tee -a "$filename" 2> /dev/null
         fi
         shift
 done
