f803c56473709b6a4cecd89b99e32dddf31c5cfe
Fazill2 <100223031+Fazill2@users.noreply.github.com>
2022-05-22 16:32:56 +0200

    Add files via upload

---
+++ morse.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+########
+declare -A morse
+morse[0]='-----'
+morse[1]='.----'
+morse[2]='..---'
+morse[3]='...--'
+morse[4]='....-'
+morse[5]='.....'
+morse[6]='-....'
+morse[7]='--...'
+morse[8]='---..'
+morse[9]='----.'
+morse[A]='.-'
+morse[B]='-...'
+morse[C]='-.-.'
+morse[D]='-..'
+morse[E]='.'
+
+########
+
+if (("$#" == "0")); then
+        echo "help"
+        exit
+fi
+
+while (("$#" > 0)); do
+        if (($1 == "-h")); then
